edd1e7f4408ca81319207caf5593eb14e144ac19
Trent Robbins <robbintt@gmail.com>
2015-12-13 19:13:47 -0800

    Added lib sample package and setup.py sample installer.

---
+++ lib/functions.py
@@ -0,0 +1,9 @@
+"""
+Functions used in the sample package.
+"""
+
+def myfunc(*args, **kwargs):
+    """ Sample Function
+    """
+    print(args)
+    print(kwargs)
---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='MyPackage',
+      version='1.0',
+      description='A Python Package',
+      author='Trent Robbins',
+      author_email='robbintt@gmail.com',
+      url='http://github.com/robbintt',
+
+      # the generic project will use lib/__init__.py
+      package_dir = {'':'lib'}
+      # packages sample:
+      # packages=['mypackagedirectory', 'mypackagedirectory.mypackage'],
+
+      # alternative, use a root directory:
+      # package_dir = {'':'mypackagedirectory', 'mypackage':'mypackagedirectory.mypackage'}
+      # note that each line implies all the packages in that directory, so 
+      # any python files in the 'mypackagedirectory' will be installed in the
+      # root space and accessible from mypackagename.mypythonfile
+
+      # simplest alternative listing individual modules:
+      # py_modules = ['mod1', 'pkg.mod2']
+      
+     )
